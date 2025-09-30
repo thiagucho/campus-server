7905a94f2e84b003c4c67100c3e1a78e54da3825
vicky10J22 <167527800+vicky10J22@users.noreply.github.com>
2025-09-30 15:52:49 -0300

    Divs

---
+++ Divs/Programacion.js
@@ -0,0 +1,8 @@
+
+function AgrSeparador(){
+    separador = `<div class = "DivSeparador"></div>"`;
+    DivContenedor = document.querySelector(".contenedor");
+    DivContenedor.innerHTML += separador;
+
+
+}
